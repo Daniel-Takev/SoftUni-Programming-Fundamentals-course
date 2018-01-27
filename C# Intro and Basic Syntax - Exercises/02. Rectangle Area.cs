2b07bd337b588c51b8c48d2283a50e523378db57
Daniel-Takev <danieltakev@gmail.com>
2018-01-27 14:16:26 +0200

    First homework for the course

---
+++ C# Intro and Basic Syntax - Exercises/02. Rectangle Area.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02._Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+
+            decimal area = a * b;
+
+            Console.WriteLine($"{area:F2}");
+        }
+    }
+}
+
